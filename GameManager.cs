032a0c47fc1c45139337a15075f8a7078958a1ec
jcaviness1 <109058376+jcaviness1@users.noreply.github.com>
2022-08-19 04:26:42 -0400

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject obstacle;
+    public Transform spawnPoint;
+    int score = 0;
+
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator SpawnObstacles()
+    {
+        while(true)
+        {
+
+            float waitTime=Random.Range(0.5f, 2f);
+
+            yield return new WaitForSeconds(waitTime);
+
+            Instantiate(obstacle, spawnPoint.position, Quaternion.identity);
+
+
+
+
+
+        }
+
+    }
+
+    void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+
+
+
+
+    public void GameStart()
+    {
+        player.SetActive(true);
+        playButton.SetActive(false);
+
+
+        StartCoroutine("SpawnObstacles");
+        InvokeRepeating("ScoreUp", 2f, 1f);
+
+
+    }
+}
